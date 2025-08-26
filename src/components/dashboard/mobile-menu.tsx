ed0624e167a7b745a28a0a838acfbbf11403b27a
federicofanini <federico.fanini9@gmail.com>
2025-08-26 16:21:06 +0200

    logo

--- src/components/dashboard/mobile-menu.tsx
+++ src/components/dashboard/mobile-menu.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Icons } from "@/components/ui/icons";
 import { Sheet, SheetContent } from "@/components/ui/sheet";
 import { useState } from "react";
 import { MainMenu } from "./main-menu";
 import { Menu } from "lucide-react";
+import Image from "next/image";
 
 export function MobileMenu() {
   const [isOpen, setOpen] = useState(false);
@@ -24,7 +24,13 @@ export function MobileMenu() {
       </div>
       <SheetContent side="left" className="border-none rounded-none -ml-4">
         <div className="ml-2 mb-8">
-          <Icons.midday className="w-10 h-10" />
+          <Image
+            src="/logo-uara.svg"
+            alt="logo"
+            width={40}
+            height={40}
+            className="w-10 h-10"
+          />
         </div>
 
         <div className="-ml-2">
